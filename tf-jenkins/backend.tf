579081c119515d7b21424709a029440756fcd9df
Smari <smari.gurunathan86@gmail.com>
2026-09-08 19:55:12 +0530

    Fix formatting of S3 bucket name in backend.tf

--- tf-jenkins/backend.tf
+++ tf-jenkins/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "smari-devops-project-1 "
+    bucket = "smari-devops-project-1"
     region = "ap-south-1"
     key    = "jenkins-server/terraform.tfstate"
   }
