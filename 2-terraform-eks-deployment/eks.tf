18a09350fd82f3d02bcfd60899ffee6d27233641
smarigurnathan <smari.gurunathan86@gmail.com>
2026-09-09 21:51:31 +0530

    Add EKS managed addons

--- 2-terraform-eks-deployment/eks.tf
+++ 2-terraform-eks-deployment/eks.tf
@@ -2,37 +2,17 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
 
-  # EKS cluster
   name               = "my-eks-cluster"
   kubernetes_version = "1.35"
 
-  # EKS endpoint
   endpoint_public_access  = true
   endpoint_private_access = true
 
-  # VPC configuration
   vpc_id     = module.my-vpc.vpc_id
   subnet_ids = module.my-vpc.private_subnets
 
-  # Give Terraform creator admin access
   enable_cluster_creator_admin_permissions = true
 
-  # EKS managed add-ons
-  cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-
-    kube-proxy = {
-      most_recent = true
-    }
-
-    vpc-cni = {
-      most_recent = true
-    }
-  }
-
-  # Managed worker nodes
   eks_managed_node_groups = {
     dev = {
       min_size     = 1
@@ -45,9 +25,27 @@ module "eks" {
     }
   }
 
-  # Tags
   tags = {
     Environment = "development"
     Application = "nginx-app"
   }
+}
+
+
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name = module.eks.cluster_name
+  addon_name   = "vpc-cni"
+  most_recent  = true
+}
+
+resource "aws_eks_addon" "kube_proxy" {
+  cluster_name = module.eks.cluster_name
+  addon_name   = "kube-proxy"
+  most_recent  = true
+}
+
+resource "aws_eks_addon" "coredns" {
+  cluster_name = module.eks.cluster_name
+  addon_name   = "coredns"
+  most_recent  = true
 }
