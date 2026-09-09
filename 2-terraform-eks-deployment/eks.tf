30070189ced083cbbaf6a0f1dd8a2e9b737122a7
smarigurnathan <smari.gurunathan86@gmail.com>
2026-09-09 21:45:08 +0530

    Add EKS managed addons

--- 2-terraform-eks-deployment/eks.tf
+++ 2-terraform-eks-deployment/eks.tf
@@ -6,15 +6,31 @@ module "eks" {
   name               = "my-eks-cluster"
   kubernetes_version = "1.35"
 
-  # Allow kubectl access through the public EKS endpoint
-  endpoint_public_access = true
+  # EKS endpoint
+  endpoint_public_access  = true
+  endpoint_private_access = true
 
   # VPC configuration
   vpc_id     = module.my-vpc.vpc_id
   subnet_ids = module.my-vpc.private_subnets
 
-  # Give the identity running Terraform admin access to the cluster
+  # Give Terraform creator admin access
   enable_cluster_creator_admin_permissions = true
+
+  # EKS managed add-ons
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+
+    kube-proxy = {
+      most_recent = true
+    }
+
+    vpc-cni = {
+      most_recent = true
+    }
+  }
 
   # Managed worker nodes
   eks_managed_node_groups = {
@@ -22,10 +38,9 @@ module "eks" {
       min_size     = 1
       max_size     = 2
       desired_size = 1
-      
+
       instance_types = ["c7i-flex.large"]
 
-      # Required/suitable for Kubernetes 1.35
       ami_type = "AL2023_x86_64_STANDARD"
     }
   }
