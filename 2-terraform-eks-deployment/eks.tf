c3e180b8c0ad289d5f9ef21adf554fad75d1be9d
smarigurnathan <smari.gurunathan86@gmail.com>
2026-09-09 21:53:50 +0530

    Use EKS module managed addons

--- 2-terraform-eks-deployment/eks.tf
+++ 2-terraform-eks-deployment/eks.tf
@@ -13,6 +13,20 @@ module "eks" {
 
   enable_cluster_creator_admin_permissions = true
 
+  addons = {
+    coredns = {
+      most_recent = true
+    }
+
+    kube-proxy = {
+      most_recent = true
+    }
+
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
   eks_managed_node_groups = {
     dev = {
       min_size     = 1
@@ -29,23 +43,4 @@ module "eks" {
     Environment = "development"
     Application = "nginx-app"
   }
-}
-
-
-resource "aws_eks_addon" "vpc_cni" {
-  cluster_name = module.eks.cluster_name
-  addon_name   = "vpc-cni"
-  most_recent  = true
-}
-
-resource "aws_eks_addon" "kube_proxy" {
-  cluster_name = module.eks.cluster_name
-  addon_name   = "kube-proxy"
-  most_recent  = true
-}
-
-resource "aws_eks_addon" "coredns" {
-  cluster_name = module.eks.cluster_name
-  addon_name   = "coredns"
-  most_recent  = true
 }
